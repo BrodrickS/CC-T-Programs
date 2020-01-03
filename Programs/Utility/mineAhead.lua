3108b51902b3192578893ea6547776a626d5754f
bts9033 <bts9033@cs.rit.edu>
2020-01-03 12:23:35 -0500

    Better numbering. FIxed off by one error

--- Programs/Utility/mineAhead.lua
+++ Programs/Utility/mineAhead.lua
@@ -11,7 +11,7 @@ if forwardMax == nil then
   forwardMax = 1
 end
 local vertMax = tonumber(arg[2])
-if vertMax == nil or vertMax = 0 then
+if vertMax == nil or vertMax == 0 then
   vertMax = 1
 end
 local horizMax = tonumber(arg[3])
@@ -36,11 +36,11 @@ else
 end
 
 -- Do forward last
-local forward = 0
+local forward = -1
 local forwardStart = smartTurtle.newPoint()
 while forward < forwardMax do
   
-  local horiz = 0
+  local horiz = -1
   local horizStart = smartTurtle.newPoint()
   while horiz < horizMax do
     
@@ -54,19 +54,25 @@ while forward < forwardMax do
     smartTurtle.returnPoint(vertStart)
     smartTurtle.removePoint(vertStart)
     
-    smartTurtle.face(horizDir)
-    smartTurtle.dig(D.FORAWRD)
-    smartTurtle.move(D.FORWARD)
-    smartTurtle.face(-horizDir)
     
     horiz = horiz + 1
+    if horiz < horizMax then
+      smartTurtle.face(horizDir)
+      smartTurtle.dig(D.FORWARD)
+      smartTurtle.move(D.FORWARD)
+      smartTurtle.face(-horizDir)
+    end
+    
   end
   smartTurtle.returnPoint(horizStart)
   smartTurtle.removePoint(horizStart)
   
-  smartTurtle.dig(D.FORWARD)
-  smartTurtle.move(D.FORWARD)
+  
   forward = forward + 1
+  if forward < forwardMax then
+    smartTurtle.dig(D.FORWARD)
+    smartTurtle.move(D.FORWARD)
+  end
   
 end
 
